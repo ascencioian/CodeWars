7205a763b388a37ef9c9f3d2ee863947594e018c
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2021-10-14 10:54:16 -0400

    new codewar

--- 8Kyu/Remove_first_and_last_character.js
+++ 8Kyu/Remove_first_and_last_character.js
@@ -1,6 +1,6 @@
-https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 
-goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+//goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
 function removeChar(str){
     let result = str.slice(1, -1) 
