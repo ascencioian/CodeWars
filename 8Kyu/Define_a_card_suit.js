f2e24f6039603e217f0d96e5aa379c88fd824cf0
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2021-10-15 10:46:50 -0400

    new codewar

---
+++ 8Kyu/Define_a_card_suit.js
@@ -0,0 +1,31 @@
+//https://www.codewars.com/kata/5a360620f28b82a711000047/train/javascript
+
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+
+function defineSuit(card) {
+    if(card.charAt(card.length-1) === '♣' ){
+     return 'clubs'
+    }
+   else if(card.charAt(card.length-1) === '♠' ){
+     return 'spades'
+    }
+   else if(card.charAt(card.length-1) === '♦' ){
+     return 'diamonds'
+    }
+   else if(card.charAt(card.length-1) === '♥' ){
+      return 'hearts'
+    } 
+}
